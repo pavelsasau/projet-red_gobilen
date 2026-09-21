2e368bd3f264ff39069bea6207340e2850e7ccb1
spavel <pavel.sasau@ynov.com>
2026-09-21 15:14:19 +0300

    gobelen

---
+++ character.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Character struct {
+	name      string
+	class     string
+	level     int
+	hp_max    int
+	hp_a      int
+	inventory []string
+}
+
+func initCharacter() Character {
+	player := Character{}
+	player.name = "test"
+	player.class = "Humains"
+	player.level = 1
+	player.hp_max = 100
+	player.hp_a = 100
+	player.inventory = []string{}
+
+	return player
+}
+
+func displayInfo(player Character) {
+	fmt.Println(player.name)
+}
+
+func main() {
+	player := initCharacter()
+
+	_ = player
+}
